299cf199bb3081bbbedb624aa890febfd1bf6b1b
Felipe-RW <felipewrubel@gmail.com>
2026-04-27 17:04:51 -0400

    Arquivo estava no local errado, local do arquivo atualizado

--- main.py
+++
@@ -1,5 +0,0 @@
-#listar produto
-lista_produto = [["banana","R$ 19.90","Código = 0121321"],["maça","R$ 17.90","Código = 0121322"],["pera","R$ 1.90","Código = 0121323"]]
-
-for i in lista_produto:
-    print(f"{i}. {lista_produto[i]}")
