1615542fca566c6129365dc24545e19745706c9a
Gabriel <gdefigueiredooliveira@gmail.com>
2026-04-27 17:34:15 -0400

    Finalizar_pedidos

---
+++ Finalizar_pedido.py
@@ -0,0 +1,25 @@
+vlrproduto=0
+vlrpago=0
+print("Escolha a forma de pagamento:")
+print("1 - Crédito")
+print("2 - Débito")
+print("3 - Pix")
+
+opcao=int(input("Digite a opção desejada: "))
+
+if opcao == 1:
+  
+    print("Pagamento no crédito selecionado.")
+    print(f"O valor final do produto é: R${vlrproduto:.2f}")
+    vlrpago=float(input('R$'))
+    print('Concluir pagamento')
+
+    if opcao == 2:
+        print("Pagamento no débito selecionado.")
+        print(f"O valor final do produto é: R${vlrproduto:.2f}")
+    else:
+        if opcao == 3:
+            print("Pagamento no Pix selecionado.")
+            print(f"O valor final do produto é: R${vlrproduto:.2f}")
+        else:
+            print("Opção inválida. Por favor, selecione uma opção válida.")
