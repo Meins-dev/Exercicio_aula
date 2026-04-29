f3719fb4f4521b5d3b165d51550d68f722b3e64b
KleberAugusto0 <kleberaugusto365@gmail.com>
2026-04-29 15:09:10 -0400

    Eu fiz o código, porem ele está incompleto

---
+++ main.py
@@ -0,0 +1,45 @@
+funcionario= []
+idades = []
+cargos= []
+senhas= []
+
+while True:
+	print("\n--Cadastro de funcionario--")
+	print("1 - Cadastrar funcionário: ")
+	print("2 - Listar funcionário: ")
+	print("3 - Buscar funcionário: ")
+	print("0 - Sair")
+
+	opcao=input("Escolha uma opção: ")
+	
+	if opcao=="1":
+		nome=input("Nome do funcionário:")
+		idade=input("Idade:")
+		cargo=input("Cargo:")
+		senha=input("Senha:")
+
+		funcionario.append(nome)
+		idades.append(idade)
+		cargos.append(cargo)
+		senhas.append (senha)
+		print("Funcionário cadastrado com sucesso.")
+
+	elif opcao=="2":
+		print (funcionario)
+
+	elif opcao=="3":
+		busca=input("Digite o nome do funcionário: ")
+		encontrado=False
+        for f in funcionario:
+		if f["nome"].lower() == busca.lower():
+		print("\nFuncionário encontrado:")
+	    print("Nome:", f[nome])
+		print("Idade:", f[idade])
+		print("Cargo:", f[cargo])
+		encontrado=True
+
+
+
+
+
+
