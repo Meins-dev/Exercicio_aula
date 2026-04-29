eb0f6a30094d9e41ce4c89a4c0dc787165c922f0
Gabriel <gdefigueiredooliveira@gmail.com>
2026-04-29 14:37:18 -0400

    Commit

--- Finalizar_pedido.py
+++ Finalizar_pedido.py
@@ -1,9 +1,10 @@
 produtos=['produto1','produto2','produto3','produto4','produto5'] #Exemplo de Produtos
-vlrunit1=30 #Valor unitário do produto 1
-vlrunit2=40 #Valor unitário do produto 2
-vlrunit3=50 #Valor unitário do produto 3
-vlrunit4=60 #Valor unitário do produto 4
-vlrproduto=vlrunit1+vlrunit2+vlrunit3+vlrunit4#Soma dos valores unitários dos produtos
+vlrunit1=float(input('Valor produto 1:R$')) #Valor unitário do produto 1
+vlrunit2=float(input('Valor produto 2:R$')) #Valor unitário do produto 2
+vlrunit3=float(input('Valor produto 3:R$')) #Valor unitário do produto 3
+vlrunit4=float(input('Valor produto 4:R$')) #Valor unitário do produto 4
+vlrunit5=float(input('Valor produto 5:R$')) #Valor unitário do produto 5
+vlrproduto=vlrunit1+vlrunit2+vlrunit3+vlrunit4+vlrunit5 #Soma dos valores unitários dos produtos
 opcao=0 #Opção de Forma de Pagamento ou de Sair
 op1=1 #Opção de Concluir Pagamento
 op2=2 #Opção de Cancelar Pagamento
@@ -21,10 +22,11 @@ while opcao<=4:
   
     print("Pagamento no crédito selecionado.")
     print(f"O valor final do produto é: R${vlrproduto:.2f}")
-    print(produtos[1],'- R$',vlrunit1)
-    print(produtos[2],'- R$',vlrunit2)
-    print(produtos[3],'- R$',vlrunit3)
-    print(produtos[4],'- R$',vlrunit4)
+    print(produtos[0],'- R$',vlrunit1)
+    print(produtos[1],'- R$',vlrunit2)
+    print(produtos[2],'- R$',vlrunit3)
+    print(produtos[3],'- R$',vlrunit4)
+    print(produtos[4],'- R$',vlrunit5)
     vlrpago=float(input('R$'))
     op=int(input('1-Concluir\n2-Cancelar\n '))
     if op==op1:
@@ -43,10 +45,11 @@ while opcao<=4:
  if opcao == 2:
     print("Pagamento no débito selecionado.")
     print(f"O valor final do produto é: R${vlrproduto:.2f}")
-    print(produtos[1],'- R$',vlrunit1)
-    print(produtos[2],'- R$',vlrunit2)
-    print(produtos[3],'- R$',vlrunit3)
-    print(produtos[4],'- R$',vlrunit4)
+    print(produtos[0],'- R$',vlrunit1)
+    print(produtos[1],'- R$',vlrunit2)
+    print(produtos[2],'- R$',vlrunit3)
+    print(produtos[3],'- R$',vlrunit4)
+    print(produtos[4],'- R$',vlrunit5)
     vlrpago=float(input('R$'))
     op=int(input('1-Concluir\n2-Cancelar\n '))
     if op==op1:
@@ -65,10 +68,11 @@ while opcao<=4:
  if opcao == 3:
     print("Pagamento no Pix selecionado.")
     print(f"O valor final do produto é: R${vlrproduto:.2f}")
-    print(produtos[1],'- R$',vlrunit1)
-    print(produtos[2],'- R$',vlrunit2)
-    print(produtos[3],'- R$',vlrunit3)
-    print(produtos[4],'- R$',vlrunit4)
+    print(produtos[0],'- R$',vlrunit1)
+    print(produtos[1],'- R$',vlrunit2)
+    print(produtos[2],'- R$',vlrunit3)
+    print(produtos[3],'- R$',vlrunit4)
+    print(produtos[4],'- R$',vlrunit5)
     vlrpago=float(input('R$'))
     op=int(input('1-Concluir\n2-Cancelar\n '))
     if op==op1:
