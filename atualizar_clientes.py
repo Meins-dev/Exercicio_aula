90f78b68669be8f8d31c33ebf320fd78f38fcb5b
viniciusspinola82-design <viniciusspinola82@gmail.com>
2026-04-29 17:13:11 -0400

    atualização para o PR

--- atualizar_clientes.py
+++ atualizar_clientes.py
@@ -1,3 +1,4 @@
+#Atualizar clientes
 clientes = []
 
 def add_cliente():
@@ -5,9 +6,9 @@ def add_cliente():
     novo_cliente = {
     "id": len(clientes) + 1,
     "nome": input("Nome: "),
-    "Email": input("Email: "),
-    "Telefone":input ("Telefone: "),
-    "Cpf": input ("Cpf: "),
+    "email": input("Email: "),
+    "telefone":input ("Telefone: "),
+    "cpf": input ("Cpf: "),
     }
     clientes.append(novo_cliente)
 def atualizar_cliente():
@@ -17,4 +18,13 @@ def atualizar_cliente():
     id_alvo = int(input("\n Digite o ID do cliente que deseja atualizar: "))
     for cliente in clientes:
         if cliente["id"] == id_alvo:
-            print ("a")
+           print (f"Atualizando:{cliente["nome"]}")
+           cliente["nome"] = input("\nDigite um novo nome: ")
+           cliente["email"] = input("\nDigite um novo email: ")
+           cliente["telefone"] = input("\nDigite um novo telefone: ")
+           cliente["cpf"] = input ("\nDigite um novo CPF: ")
+           print ("Atualizado! \n")
+           return
+print ("Cliente não encontrado.")
+add_cliente() # isso vai disparar a tela de cadastro
+atualizar_cliente() # isso vai disparar a tela de atualização.
