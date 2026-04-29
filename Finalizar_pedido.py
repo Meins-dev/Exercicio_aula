dfb3e1013cd0ad001f708b24393e6ac6eef83ea7
Gabriel <gdefigueiredooliveira@gmail.com>
2026-04-29 14:31:10 -0400

    Commit

--- Finalizar_pedido.py
+++ Finalizar_pedido.py
@@ -1,41 +1,90 @@
-vlrproduto=0
-vlrpago=0
-print("Escolha a forma de pagamento:")
-print("1 - Crédito")
-print("2 - Débito")
-print("3 - Pix")
-
-opcao=int(input("Digite a opção desejada: "))
-
-if opcao == 1:
+produtos=['produto1','produto2','produto3','produto4','produto5'] #Exemplo de Produtos
+vlrunit1=30 #Valor unitário do produto 1
+vlrunit2=40 #Valor unitário do produto 2
+vlrunit3=50 #Valor unitário do produto 3
+vlrunit4=60 #Valor unitário do produto 4
+vlrproduto=vlrunit1+vlrunit2+vlrunit3+vlrunit4#Soma dos valores unitários dos produtos
+opcao=0 #Opção de Forma de Pagamento ou de Sair
+op1=1 #Opção de Concluir Pagamento
+op2=2 #Opção de Cancelar Pagamento
+while opcao<=4: 
+ print("Escolha a forma de pagamento:")
+ print("1 - Crédito")
+ print("2 - Débito")
+ print("3 - Pix")
+ print("4 - Sair")
+ opcao=int(input("Digite a opção desejada: "))
+ if opcao > 4:
+            print("Opção inválida.")
+            continue
+ if opcao == 1:
   
     print("Pagamento no crédito selecionado.")
     print(f"O valor final do produto é: R${vlrproduto:.2f}")
+    print(produtos[1],'- R$',vlrunit1)
+    print(produtos[2],'- R$',vlrunit2)
+    print(produtos[3],'- R$',vlrunit3)
+    print(produtos[4],'- R$',vlrunit4)
     vlrpago=float(input('R$'))
-    print('Concluir pagamento')
-    if vlrpago == vlrproduto:
-        print('Pagamento concluído com sucesso!')
+    op=int(input('1-Concluir\n2-Cancelar\n '))
+    if op==op1:
+        if vlrpago==vlrproduto:
+         print('Valor R$',vlrpago)
+         print('Pagamento concluído com sucesso!')
+         break
+        else:
+         print('Valor pago insuficiente. Pagamento não concluído.')
+    if op==op2:
+        print('Pagamento Cancelado')
+        break
     else:
-        print('Valor pago insuficiente. Pagamento não concluído.')
-
-if opcao == 2:
+        print('Esta opção não existe')
+        break
+ if opcao == 2:
     print("Pagamento no débito selecionado.")
     print(f"O valor final do produto é: R${vlrproduto:.2f}")
+    print(produtos[1],'- R$',vlrunit1)
+    print(produtos[2],'- R$',vlrunit2)
+    print(produtos[3],'- R$',vlrunit3)
+    print(produtos[4],'- R$',vlrunit4)
     vlrpago=float(input('R$'))
-    print('Concluir pagamento')
-    if vlrpago == vlrproduto:
-        print('Pagamento concluído com sucesso!')
+    op=int(input('1-Concluir\n2-Cancelar\n '))
+    if op==op1:
+        if vlrpago==vlrproduto:
+         print('Valor R$',vlrpago)
+         print('Pagamento concluído com sucesso!')
+         break
+        else:
+         print('Valor pago insuficiente. Pagamento não concluído.')
+    if op==op2:
+        print('Pagamento Cancelado')
+        break
     else:
-        print('Valor pago insuficiente. Pagamento não concluído.')
-
-if opcao == 3:
+        print('Esta opção não existe')
+        break
+ if opcao == 3:
     print("Pagamento no Pix selecionado.")
     print(f"O valor final do produto é: R${vlrproduto:.2f}")
+    print(produtos[1],'- R$',vlrunit1)
+    print(produtos[2],'- R$',vlrunit2)
+    print(produtos[3],'- R$',vlrunit3)
+    print(produtos[4],'- R$',vlrunit4)
     vlrpago=float(input('R$'))
-    print('Concluir pagamento')
-    if vlrpago == vlrproduto:
-        print('Pagamento concluído com sucesso!')
+    op=int(input('1-Concluir\n2-Cancelar\n '))
+    if op==op1:
+        if vlrpago==vlrproduto:
+         print('Valor R$',vlrpago)
+         print('Pagamento concluído com sucesso!')
+         break
+        else:
+         print('Valor pago insuficiente. Pagamento não concluído.')
+    if op==op2:
+        print('Pagamento Cancelado')
+        break
     else:
-        print('Valor pago insuficiente. Pagamento não concluído.')
-else:
-            print("Opção inválida. Por favor, selecione uma opção válida.")
+        print('Esta opção não existe')
+        break
+ if opcao==4:
+    print("Saindo do programa. Obrigado por comprar conosco!")
+    break
+ 
