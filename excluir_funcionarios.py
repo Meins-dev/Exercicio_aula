4494d6c68b814f97fcb4cefe9c96df65a2d3ae6e
Gabriel <gdefigueiredooliveira@gmail.com>
2026-04-29 16:06:51 -0400

    files adcionados

---
+++ excluir_funcionarios.py
@@ -0,0 +1,26 @@
+escolha = 1
+funcionarios = []
+idades = []
+cargos = []
+senhas = []
+
+while escolha != 0:
+
+    escolha = str (input ("Digite 1 para selecionar um funcionario ou 0 para voltar: "))
+
+    if escolha == "1":
+        excluir = str (input ("\nDigite o nome do funcionário que deseja excluir: "))
+        if excluir in funcionarios:
+            x = funcionarios.index (excluir)
+            funcionarios.pop (x)
+            idades.pop (x)
+            cargos.pop (x)
+            senhas.pop (x)
+            print ("\nCliente Excluido!\n")
+        else:
+            print ("\nFuncuinário não encontrado!\n")
+    elif escolha == "0":
+        print ("\nRetornando ao Menu...\n")
+        break
+    else:
+        print ("\nOpção Inválida\n")
