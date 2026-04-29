9dd206afd4124db5424a650c443e4f4fd9faae74
PedroMenani <pedromenani03@gmail.com>
2026-04-29 16:50:53 -0400

    Juntando o menu ao cadastro de produtos e listagem, corrigindo os erros e arrumando o código

--- cadastrar_produtos.py
+++ cadastrar_produtos.py
@@ -1,24 +1,54 @@
 #Cadastro de Produtos
 lista_produto=[] #lista principal
-loop_cadastro = True #Fluxo do código. Garante a repetição do cadastro
-while loop_cadastro == True:
-    informacoes=[] #sublista dos produtos. Forma de separar cada produto com seu respectivo preço e código
-    produto_nome=str(input("Digite o nome do produto:\n"))
-    informacoes.append(produto_nome)
-    produto_preco=float(input("Digite o preço:\n"))
-    informacoes.append(produto_preco)
-    produto_cod = str(input("Digite o código:\n"))
-    informacoes.append(produto_cod)
-    lista_produto.append(informacoes)
-    print("Produto cadastrado com sucesso")
-    while True:
-        loop = int(input("Deseja cadastrar outro?\n1 - Sim\n2 - Não \n")) #Escolha de repetição
-        if loop == 1:
-            loop_cadastro = True
-            break
-        elif loop == 2:
-            loop_cadastro = False
-            break
+informacoes=[] #sublista dos produtos. Forma de separar cada produto com seu respectivo preço e código
+while True:
+    print('''
+    +----------PRODUTOS----------+
+    |  1-Cadastrar produtos      |
+    |  2-Listar produtos         |
+    |  3-Excluir produtos        |
+    |  4-Finalizar produtos      |
+    |  0-Sair                    |
+    +----------------------------+
+    ''')
+    menuProdutos = int(input("Selecione uma opção baseado no número do menu dos produtos: "))
+
+    if menuProdutos == 1: #Cadastrar produtos
+        print("\n+--------------+\n")
+        print("Estou cadastrando os produtos.")
+
+        
+        loop_cadastro = True #Fluxo do código. Garante a repetição do cadastro
+        while loop_cadastro == True:
+            
+            produto_nome=str(input("Digite o nome do produto:\n"))
+            informacoes.append(produto_nome)
+            produto_preco=float(input("Digite o preço:\n"))
+            informacoes.append(produto_preco)
+            produto_cod = str(input("Digite o código:\n"))
+            informacoes.append(produto_cod)
+            lista_produto.append(informacoes)
+            print("Produto cadastrado com sucesso")
+
+            while True:
+                loop = int(input("Deseja cadastrar outro?\n1 - Sim\n2 - Não \n")) #Escolha de repetição
+                if loop == 1:
+                    loop_cadastro = True
+                    break
+                elif loop == 2:
+                    loop_cadastro = False
+                    break
+                else:
+                    print("Opção Inválida.")
+                    continue
+
+    elif menuProdutos == 2: #Listar produtos
+        print("\n+--------------+\n")
+        print("Estou listando os produtos.")
+
+        if len(lista_produto) > 0:
+                for i in range(len(lista_produto)):
+                    print(f"{i + 1}. Nome do Produto: {lista_produto[i][0]} - Preço: R$ {lista_produto[i][1]:.2f} - Código: {lista_produto[i][2]}")
+
         else:
-            print("Opção Inválida.")
-            continue
+                print("Nenhum produto cadastrado.")
