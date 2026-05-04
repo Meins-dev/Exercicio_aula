16aaf0fd0fb1f8365ab8fcd53e4f9d78d2aca1f9
Mateus-Freitas7 <senacmateus8@gmail.com>
2026-05-04 15:59:23 -0400

    Código para excluir pedidos do carrinho

--- excluir_pedidos.py
+++ excluir_pedidos.py
@@ -1,13 +1,13 @@
 
 #Excluir pedidos
-
-
-
-lista = ["carro","moto","bicicleta"] #Exemplo
-
-item = (input("Digite o item que deseja excluir:")) #Recebe o item desejado para excluir
-
-indice = lista.index(item)
-lista.pop (indice)
-print("\nItem excluído")
-print("\nCarrinho:",item)
+#Precisa da lista carrinho para funcionar
+while True:
+      if len(carrinho) != 0:
+            escolha =  int(input("Digite o código do produto que deseja excluir:"))
+            excluido = carrinho.pop(escolha)
+            print(f"\nItem {excluido[0]} excluído")
+            sair = input("Você deseja parar de excluir produtos? (s = sim / n = não)")
+            if sair.lower() == 's':
+                  break
+      else:
+            print("Nenhum produto no carrinho.")
