03ae53848007bc61511bfd562d7d29f63fab9b8f
MuriloMedeiros7 <murilo91321@gmail.com>
2026-05-04 15:10:33 -0400

    Finalizado o feature de somar os prdutos do carrinho

--- somar_produtos.py
+++ somar_produtos.py
@@ -1,24 +1,5 @@
-lista_produto = []
-contador = 0
-
-quantidade = int(input("Qual a quantidade de produtos? "))
-
-while contador < quantidade:
-    preco = float(input("Digite o preço: "))
-    
-    if preco >= 0:
-        if preco == 0:
-            print("O valor do produto é 0")
-        lista_produto.append(preco)
-        contador += 1 
-    else:
-        print("Valor inválido")
-
-valor_total = 0
-indice = 0
-
-while indice < len(lista_produto):
-    valor_total += lista_produto[indice]
-    indice += 1
-
-print("Valor total:", valor_total)
+#Pro código funcionar precisa da variavel carrinho que vai vir como uma lista
+soma = 0
+for item in carrinho:
+    soma += item[1]
+print(f"O valor da soma é: R$ {soma:.2f}")
