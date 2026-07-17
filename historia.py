d411792a6c3856450dfd529137e17454f0426aa9
Adalberto Marinho <adalbertomarinho2010@gmail.com>
2026-07-16 23:04:02 -0400

    Corrigindo alguns erros de sintaxe

--- historia.py
+++ historia.py
@@ -1,3 +1,3 @@
-print(=== Início da História ===)
-print(Era uma vez, dois programadores tentando resolver um problema no Git...)
-print(O Pokemon Gengar, escondeu o código original do repositório)
+print("=== Início da História ===")
+print("Era uma vez, dois programadores tentando resolver um problema no Git...")
+print("O Pokemon Gengar, escondeu o código original do repositório.")
